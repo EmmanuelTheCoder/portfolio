a0019373aaa58abfb3388b8a83efe42f552b07ab
Emmanuel Aiyenigba <55356373+EmmanuelTheCoder@users.noreply.github.com>
2022-03-11 12:47:24 +0100

    updated resume

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () =>{
         <div className="navbar">
             <a href="#contact">Contact</a> 
             <a href="#projects">Projects</a>
-            <a href="https://www.dropbox.com/s/b92z0wv7yorgkd5/resume.pdf?dl=0">Resume</a>
+            <a href="https://www.dropbox.com/s/k8y1u11lr72b1gx/Emmanuel%20Aiyenigba%27s%20resume.docx?dl=0">Resume</a>
             <a href="#intro">About</a>
         </div>
         </div>
